1995d66e848999dc2f75a0825a5ec84ab45126f3
Live session user <secoy@live.com>
2017-06-09 10:46:22 +0200

    lessons group by day ?

--- projetMJC/src/AppBundle/Repository/LessonRepository.php
+++ projetMJC/src/AppBundle/Repository/LessonRepository.php
@@ -70,7 +70,7 @@ class LessonRepository extends \Doctrine\ORM\EntityRepository
         $query = $this->getEntityManager()->createQuery(
           'SELECT l FROM AppBundle:Lesson l
           JOIN AppBundle:Subscription s
-          WHERE s.teacherId = ?3 OR s.student = ?3
+          WHERE s.teacher = ?3 OR s.student = ?3
           AND l.startAt> ?1 AND l.startAt< ?2 ORDER BY s.startAt');
           $query->setParameter(1, $today0->format('Y-m-d 00:00:00'));
           $query->setParameter(2, $today1->format('Y-m-d 23:59:59'));
@@ -83,19 +83,35 @@ class LessonRepository extends \Doctrine\ORM\EntityRepository
         public function lessonsNowAfter($id)
         {
             $today = new \DateTime();
+            $finishDate = new \DateTime();
 
-                $query = $this->getEntityManager()->createQuery(
-                  'SELECT l.startAt,
-                  COUNT(DISTINCT(l.id))
-                   FROM AppBundle:Lesson l
-                  JOIN AppBundle:Subscription s
-                  WHERE (s.teacher = ?1 OR s.student = ?1)
-                  AND l.startAt> ?2 group by l.startAt ORDER BY l.startAt ASC');
-                  $query->setParameter(1, $id);
-                  $query->setParameter(2, $today);
-                $result = $query->getResult();
-                return $result;
+          $query = $this->createQueryBuilder('l')
+            // ->select('l')
+            ->select('count(l) as nb, SUBSTRING(l.startAt, 9, 2) as day')
+            ->join('AppBundle:Subscription' ,'s')
+            ->where('s.teacher = ?1 OR s.student = ?1')
+            ->andwhere('l.startAt > ?2')
+            ->andwhere('l.startAt < ?3')
+
+            ->groupBy('day')
+            ->setParameter(1, $id)
+            ->setParameter(2, $today)
+            ->setParameter(3, $finishDate->format('2017-06-14 23:59:59'))
+            ->getQuery()->getResult();
+//
+//
+
+            return $query;
+          //   count(DISTINCT(lesson.id))
+          //   FROM AppBundle:Lesson l
+          //   JOIN AppBundle:Subscription s
+          //   WHERE (s.teacher = ?1 OR s.student = ?1)
+          //   AND DATE('l.startAt') > ?2 group by convert(varchar, l.startAt, 101)');
+          // $query->setParameter(1, $id);
+          // $query->setParameter(2, $today);
+
+
 
         }
-        // SELECT DATE(lesson.startAt), count(DISTINCT(lesson.id)) FROM `lesson` inner join subscription where lesson.startAt > DATE(Now()) and (subscription.teacher_id = 5 or subscription.student_id = 5) group by DATE(lesson.startAt) ORDER BY DATE(lesson.startAt) ASC
+         // SELECT DATE(lesson.startAt), count(DISTINCT(lesson.id)) FROM `lesson` inner join subscription where lesson.startAt > DATE(Now()) and (subscription.teacher_id = 5 or subscription.student_id = 5) group by DATE(lesson.startAt) ORDER BY DATE(lesson.startAt) ASC
 }
