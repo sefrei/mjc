b990367f5aafe7654faf2fb76fdfa86820794be0
corke <corke.aj@gmail.com>
2017-09-15 21:04:46 +0200

    remove

--- projetMJC/front/app/src/store/reducer.js
+++ projetMJC/front/app/src/store/reducer.js
@@ -240,13 +240,6 @@ export const selectActivity = (state, props) => {
   ));
   if (activitySelected.length) {
     return activitySelected[0];
-  }else {
-    activitySelected = state.activitiesNotif.filter(activity => (
-      activity.activity_id === id
-    ));
-    if (activitySelected.length) {
-      return activitySelected[0];
-    }
   }
   return null;
 };
